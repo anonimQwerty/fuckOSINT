82d20775fb529db3221c05aa471cf42f91f01e28
anonimQwerty <61067633+anonimQwerty@users.noreply.github.com>
2020-04-19 21:35:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,18 @@
-import os, fuckOSINT_byname
+import fuckOSINT_byname
+# number
+import libs.logo as logo
 
 modes=['1.пробив по имени ']
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def main():
-	fuckOSINT_byname.logo()
+	logo.logo()
 	for i in modes:
 		print(i)
 	m=int(input('choose mode '))
 	if m==1:
 		fuckOSINT_byname.main()
-
-
-
-
-if __name__ == '__main__':
-	main()
-	for i in modes:
-		print(i)
-	m=int(input('choose mode '))
-	if m==1:
-		fuckOSINT_byname.main()
-
-
-
+	elif m==2:
+		number.main()
 
 if __name__ == '__main__':
 	main()
