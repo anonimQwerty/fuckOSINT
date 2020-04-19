7d76b4b038173a07c2506d1775b7eccda173497b
anonimQwerty <61067633+anonimQwerty@users.noreply.github.com>
2020-04-19 21:37:51 +0300

    Add files via upload

---
+++ libs/generate_requests.py
@@ -0,0 +1,71 @@
+﻿import users_agents as users_agents
+import random, requests, json
+
+
+
+
+
+
+
+
+def generate_request():
+	user=random.choice(users_agents.headers_useragents)
+	requests_list={
+'name':['instagram', 'vk'],
+'link':['https://www.instagram.com/'],
+'headers':[
+{
+	'Accept':'*/*',
+	'Accept-Encoding':'gzip, deflate, br',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+'Cache-Control':'no-cache',
+'Connection':'keep-alive',
+'Content-Length':'293',
+'Content-Type':'application/x-www-form-urlencoded',
+'Cookie':'ig_cb=1; ig_did=FEA830C5-E633-42C9-AFE0-03942A43257E; csrftoken=5bTq2aCiosADYwDx5RvyuV2LqgYUCoXZ; rur=VLL; mid=XpxVVQAEAAE_-uDoKOO4k2X3uu4T',
+'DNT':'1',
+'Host':'www.instagram.com',
+'Pragma':'no-cache',
+'Referer':'https://www.instagram.com/',
+'User-Agent':user,
+'X-CSRFToken':'5bTq2aCiosADYwDx5RvyuV2LqgYUCoXZ',
+'X-IG-App-ID':'936619743392459',
+'X-IG-WWW-Claim':'0',
+'X-Instagram-AJAX':'cc6f59f85f33',
+'X-Requested-With':'XMLHttpRequest',
+
+
+
+}
+
+
+
+
+],
+'data':[
+{
+'enc_password':'#PWD_INSTAGRAM_BROWSER:6:1587303903:AZtQAH4dKKi93th04DCNSVaQhS3rbbNrXfisGYgLUOg1zHg/T7s6guC3UHVPiZRVtfjwrmmsbpGYJurt/Qa/gFlh8gBBQhx0M/IEQQy+bp9k9jGtfs7NvD8eghz6Itmf2uMGCrbLwMLZ8IDI',
+'optIntoOneTap':'false',
+'password':'rttyhyuy',
+'queryParams':'{}',
+'username':'+380987262484'}
+
+],
+'answer':[]
+
+
+	}
+	return requests_list
+
+	
+a=generate_request()
+s=requests.Session()
+site=a['link'][0]
+headers=a['headers'][0]
+data=a['data'][0]
+r=s.post(site, headers=headers, data=data)
+
+#print(r.content)
+#print(r.text)
+#print(r.status_code)
+print(r.content)
---
+++ libs/users_agents.py
@@ -0,0 +1,36 @@
+import random
+
+header_list=[]
+headers_useragents=['Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3',
+'Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)',
+'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)',
+'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1',
+'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1',
+'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)',
+'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)',
+'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)',
+'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)',
+'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
+'Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)',
+'Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51'
+]
+
+def randomString(size):
+	out_str = ''
+	for i in range(0, size):
+		a = random.randint(65, 90)
+		out_str += chr(a)
+	return(out_str)
+
+def initHeaders():
+	headers = {
+				'User-Agent': random.choice(headers_useragents),
+				'Cache-Control': 'no-cache',
+				'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+				'Referer': 'http://www.google.com/?q=' + randomString(random.randint(5,10)),
+				'Keep-Alive': str(random.randint(110,120)),
+				'Connection': 'keep-alive'
+				}
+
+	return headers
+
